f3b348443b5d39f72affd924c212c4d76dce02de
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-08-02 03:35:04 -0500

    ✨ setup nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/eslint',
+    '@nuxt/ui',
   ],
   eslint: {
     config: {
