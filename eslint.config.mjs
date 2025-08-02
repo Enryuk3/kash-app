6c997a9ca5d485768b80ab4b3add931b9d144d6c
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-08-02 03:10:44 -0500

    ✨ setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(antfu({
+  type: 'app',
+  vue: true,
+}, {
+  rules: {
+    'ts/consistent-type-definitions': ['error', 'type'],
+    'no-console': ['warn'],
+    'antfu/no-top-level-await': ['off'],
+    'node/prefer-global/process': ['off'],
+    'node/no-process-env': ['error'],
+    'perfectionist/sort-imports': ['error', {
+      tsconfigRootDir: '.',
+    }],
+    'unicorn/filename-case': ['error', {
+      case: 'kebabCase',
+      ignore: ['README.md'],
+    }],
+  },
+}))
