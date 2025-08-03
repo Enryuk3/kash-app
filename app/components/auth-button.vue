adb26571b0267c36a71fbd93d20478021ea2907f
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-08-03 18:32:41 -0500

    ✨ add auth state / sign out

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -1,17 +1,37 @@
 <script lang="ts" setup>
+import type { DropdownMenuItem } from '@nuxt/ui'
+
 const authStore = useAuthStore()
+
+const items = ref<DropdownMenuItem[]>([
+  {
+    label: 'Salir',
+    icon: 'i-tabler-logout-2',
+    to: '/sign-out',
+  },
+])
 </script>
 
 <template>
+  <UDropdownMenu
+    v-if="!authStore.loading && authStore.user"
+    :items="items"
+    :content="{
+      align: 'end',
+      side: 'bottom',
+      sideOffset: 8,
+    }"
+    :ui="{ content: 'w-44' }"
+  >
+    <UButton v-if="authStore.user.image" :label="authStore.user.name" :avatar="{ src: authStore.user.image }" color="neutral" variant="outline" />
+  </UDropdownMenu>
   <UButton
+    v-else
     label="Iniciar Sesión"
     trailing-icon="i-tabler-brand-github"
     class="hidden sm:inline-flex"
     :loading="authStore.loading"
+    :disabled="authStore.loading"
     @click="authStore.signIn"
   />
 </template>
-
-<style>
-
-</style>
---
+++ app/pages/sign-out.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.signOut()
+})
+</script>
+
+<template>
+  <div class="min-h-screen bg-white dark:bg-gray-900 flex items-center justify-center p-4">
+    <div class="max-w-md w-full">
+      <div class="text-center">
+        <UIcon name="i-tabler-brand-github" class="w-12 h-12 text-primary-500 mx-auto mb-4" />
+
+        <UButton color="neutral" loading-auto label="Cerrando sesión..." />
+      </div>
+    </div>
+  </div>
+</template>
