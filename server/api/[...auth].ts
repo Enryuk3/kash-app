e240b72ccd5e9393559b524be28b76cd9270c5d4
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-08-04 06:17:14 -0500

    ✨ add database url to env schema

--- server/api/[...auth].ts
+++ server/api/[...auth].ts
@@ -3,4 +3,3 @@ import { auth } from '~/lib/auth'
 export default defineEventHandler((event) => {
   return auth.handler(toWebRequest(event))
 })
-// https://youtu.be/DK93dqmJJYg?si=NfFv3Dw2lrCaB9dJ&t=6140
